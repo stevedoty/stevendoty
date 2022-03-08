ec460fedfe45b55e1f03e9e8303ea4ac474e12f2
Steve Doty <stevedoty16@gmail.com>
2022-03-08 09:42:04 -0800

    hero layout without animations

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Hero = () => {
   return (
     <div className="Hero">
-        <div id="hero-left"></div>
-        <div id="hero-right"></div>
+        <div id="hero-left" className="hero-background"></div>
+        <div id="hero-right" className="hero-background"></div>
         <div id="hero-overlay">
           <h1 id="hero-overlay-left">Welcome</h1>
           <h1 id="hero-overlay-right">.</h1>
