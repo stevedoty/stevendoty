6529478cd5301e24cb340399ec01c17e0af579ea
Steve Doty <stevedoty16@gmail.com>
2022-03-08 08:43:30 -0800

    begin basic layout

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+
+    </div>
+  );
+}
+
+export default Navbar;
