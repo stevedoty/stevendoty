9c11f13933d69b2f0ae16ba6f05868bfee5a8c67
Steve Doty <stevedoty16@gmail.com>
2022-03-08 09:48:07 -0800

    begin nav refactor left/right components

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div className="Navbar">
-      <ul>
+      <div id="nav-left">
         <h1>Steven Doty</h1>
+      </div>
+      <div id="nav-right">
         <h1>About</h1>
         <h1>Tech</h1>
         <h1>Projects</h1>
         <h1>Contact</h1>
-      </ul>
+      </div>
     </div>
   );
 }
