57bb2f41502f569b3388b141b4905f1d419c3a2c
Steve Doty <stevedoty16@gmail.com>
2022-03-08 09:36:12 -0800

    hero cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,8 @@ import Hero from './components/hero/hero'
 function App() {
   return (
     <div className="App">
-    <Navbar/>
-    <Hero/>
-      <header className="App-header">
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Hero/>
     </div>
   );
 }
