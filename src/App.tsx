c4dcff80399345c102a6c32b09d24e4204cfd918
Steve Doty <stevedoty16@gmail.com>
2022-03-08 09:28:29 -0800

    begin hero componentbasic hero layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 import Navbar from './components/navbar/navbar'
+import Hero from './components/hero/hero'
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <Hero/>
       <header className="App-header">
 
         <img src={logo} className="App-logo" alt="logo" />
---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className="Hero">
+        <div id="hero-left"></div>
+        <div id="hero-right"></div>
+        <div id="hero-overlay">
+          <h1 id="hero-overlay-left">Welcome</h1>
+          <h1 id="hero-overlay-right">.</h1>
+        </div>
+    </div>
+  );
+}
+
+export default Hero;
