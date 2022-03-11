2f904a851af6007314915159e2bd041618fadb88
Steve Doty <stevedoty16@gmail.com>
2022-03-11 10:09:58 -0800

    basic about section layout

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -5,7 +5,7 @@ const About = () => {
     <div className="About">
       <div id="about-container">
         <h1>Steven Doty</h1>
-        <h2>Software Engineer</h2>
+        <h3>Software Engineer</h3>
         <p>
           After many years of learning Electrical Engineering, I volunteered in India, bathed elephants in Cambodia, and witnessed various other cultures in Vietnam, Italy, London, and Sri Lanka.
           My career experiences range from fine finish woodworking to being a lifeguard, from being a server at a restaurant to being a ranch hand.
