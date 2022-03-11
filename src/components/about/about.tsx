8ecb950a2e7b89a864d9ead306c10cf812fd9c94
Steve Doty <stevedoty16@gmail.com>
2022-03-11 09:49:13 -0800

    create about.tsx

---
+++ src/components/about/about.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="About">
+      <div id="about-container">
+        <h1>Steven Doty</h1>
+        <h2>Software Engineer</h2>
+        <p>
+          After many years of learning Electrical Engineering, I volunteered in India, bathed elephants in Cambodia, and witnessed various other cultures in Vietnam, Italy, London, and Sri Lanka.
+          My career experiences range from fine finish woodworking to being a lifeguard, from being a server at a restaurant to being a ranch hand.
+          Over the years I have learned many things and most recently I learned Web Development.
+          It started as a hobby, turned into a passion, and now I'm choosing to make it a career.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
