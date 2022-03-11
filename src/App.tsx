3b5233680f17aa6b4878017ecbf518a345a17eb1
Steve Doty <stevedoty16@gmail.com>
2022-03-11 09:50:20 -0800

    about in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 
 import Navbar from './components/navbar/navbar'
 import Hero from './components/hero/hero'
+import About from './components/about/about'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Hero/>
+      <About/>
     </div>
   );
 }
