86d0e0c0bee7cc67c626a6e9312975c04cc34aaa
Steve Doty <stevedoty16@gmail.com>
2022-03-12 07:55:03 -0800

    technologies basic layout

--- src/components/technologies/technologies.tsx
+++ src/components/technologies/technologies.tsx
@@ -3,7 +3,43 @@ import React from 'react';
 const Technologies = () => {
   return (
     <div className="Technologies">
-
+      <div id="technologies-container">
+        <h1>Technologies</h1>
+        <div id="technologies-list-container">
+          <div>
+            <div>Title</div>
+            <ul>
+              <li>item1</li>
+              <li>item2</li>
+              <li>item3</li>
+            </ul>
+          </div>
+          <div>
+            <div>Title</div>
+            <ul>
+              <li>item1</li>
+              <li>item2</li>
+              <li>item3</li>
+            </ul>
+          </div>
+          <div>
+            <div>Title</div>
+            <ul>
+              <li>item1</li>
+              <li>item2</li>
+              <li>item3</li>
+            </ul>
+          </div>
+          <div>
+            <div>Title</div>
+            <ul>
+              <li>item1</li>
+              <li>item2</li>
+              <li>item3</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
