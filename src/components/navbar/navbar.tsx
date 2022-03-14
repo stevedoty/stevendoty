5733493d2203145d15dac98204c38731dd66998f
Steve Doty <stevedoty16@gmail.com>
2022-03-14 06:40:12 -0700

    nav item hover color grey

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,13 +4,13 @@ const Navbar = () => {
   return (
     <div className="Navbar">
       <div id="nav-left">
-        <h3>Steven Doty</h3>
+        <h3 className="navbar-item">Steven Doty</h3>
       </div>
       <div id="nav-right">
-        <h3>About</h3>
-        <h3>Tech</h3>
-        <h3>Projects</h3>
-        <h3>Contact</h3>
+        <h3 className="navbar-item">About</h3>
+        <h3 className="navbar-item">Tech</h3>
+        <h3 className="navbar-item">Projects</h3>
+        <h3 className="navbar-item">Contact</h3>
       </div>
     </div>
   );
