4bcd98e32a5d7073ec91b886f09b4dd31ecd5ffb
Steve Doty <stevedoty16@gmail.com>
2022-03-14 07:29:12 -0700

    about secton margins and justify paragraph

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -4,9 +4,9 @@ const About = () => {
   return (
     <div className="About">
       <div id="about-container">
-        <h1>Steven Doty</h1>
-        <h3>Software Engineer</h3>
-        <p>
+        <h1 className="about-header">Steven Doty</h1>
+        <h3 className="about-subheader">Software Engineer</h3>
+        <p className="about-paragraph">
           After many years of learning Electrical Engineering, I volunteered in India, bathed elephants in Cambodia, and witnessed various other cultures in Vietnam, Italy, London, and Sri Lanka.
           My career experiences range from fine finish woodworking to being a lifeguard, from being a server at a restaurant to being a ranch hand.
           Over the years I have learned many things and most recently I learned Web Development.
