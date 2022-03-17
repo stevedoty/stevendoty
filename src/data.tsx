a7b2f38a794055f6ff5b1ac0990fc472b3fe1c3c
Steve Doty <stevedoty16@gmail.com>
2022-03-17 09:29:09 -0700

    create main database

---
+++ src/data.tsx
@@ -0,0 +1,52 @@
+export const MAIN_DATA = {
+  TECHNOLOGIES_DATA:[
+    {
+      title:"Front End",
+      list:[
+        "Javascript",
+        "Typescript",
+        "(ES7)",
+        "React",
+        "HTML5",
+        "CSS3",
+        "Flexbox",
+        "CSS Grid",
+        "jQuery",
+        "Bootstrap",
+        "MaterialUI"
+      ]
+    },
+    {
+      title:"Back End",
+      list:[
+        "Node",
+        "Express",
+        "MySQL",
+        "MongoDB",
+        "Python"
+      ]
+    },
+    {
+      title:"Testing/Deployment",
+      list:[
+        "Docker",
+        "Jest",
+        "Firebase",
+        "Google Cloud Server",
+        "(CI/CD)"
+      ]
+    },
+    {
+      title:"Developer Tools",
+      list:[
+        "Vim",
+        "Git",
+        "Webpack",
+        "Babel",
+        "npm",
+        "yarn",
+        "Agile methodology"
+      ]
+    },
+  ]
+}
